0a9004c8b538cd9a7f36edc4d2fdc972da16efa8
Igor Popov <dedepete@mail.ru>
2017-01-19 23:32:02 +0300

    Removed unused file.

--- FreeLauncher/Properties/AssemblyInfo.cs
+++ FreeLauncher/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.5.1053")]
-[assembly: AssemblyFileVersion("0.1.5.1053")]
+[assembly: AssemblyVersion("0.1.5.1058")]
+[assembly: AssemblyFileVersion("0.1.5.1058")]
 [assembly: AssemblyInformationalVersion("%version3%_%branch%.%shorthash%")]
